44cee89860457e41fb537a60d718f4f6a24f6a4a
david <dsolanoreyes@my.wctc.edu>
2022-02-07 05:08:39 -0600

    done

---
+++ application.js
@@ -0,0 +1,19 @@
+function nextTab(){
+    $('[href="#nav-personalInfo"]').tab('show');
+}
+
+function nextTab2(){
+    $('[href="#nav-position"]').tab('show');
+}
+
+function nextTab3(){
+    $('[href="#nav-workexp"]').tab('show');
+}
+
+function nextTab4(){
+    $('[href="#nav-education"]').tab('show');
+}
+
+function nextTab5(){
+    $('[href="#nav-generalInfo"]').tab('show');
+}
